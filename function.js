5e32c4334e7076de645a35e9df3b5ba7ff1b2a16
yudiz_hirenpatoliya <hiren.patoliya@yudiz.com>
2023-05-10 16:15:29 +0530

    update test script

--- function.js
+++ function.js
@@ -2,14 +2,13 @@ const cube = {}
 
 cube.getCubesideLength = (length) => {
     return length
-    
 }
 
-cube.getCubesurfaceArea = () => {
+cube.getCubesurfaceArea = (length) => {
     return length*length*6
 }
 
-cube.getCubevolume = () => {
+cube.getCubevolume = (length) => {
     return Math.pow(length,3)
 }
 
